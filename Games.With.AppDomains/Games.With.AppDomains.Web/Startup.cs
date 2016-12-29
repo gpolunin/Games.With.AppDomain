dd098e2f5ca09ce8afcb19c24b900133c014b71d
George Polunin <polunin@targetprocess.com>
2016-12-29 18:44:57 +0300

    initial

---
+++ Games.With.AppDomains/Games.With.AppDomains.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Games.With.AppDomains.Web.Startup))]
+namespace Games.With.AppDomains.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
